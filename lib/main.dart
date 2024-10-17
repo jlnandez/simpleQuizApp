0a2aa03eb6622ab0c1533e1bee910116f9b13e3c
jlnandez <54343551+jlnandez@users.noreply.github.com>
2024-10-16 18:50:37 -0700

    Db connect

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/question_model.dart';
 import './screens/home_screen.dart';
+import './models/db_connect.dart';
 
 void main(){
+  
+  var db = Dbconnect();
+
+  //db.addQuestion(
+  //  Question(id: '20', title: 'What is 2 x 100 ?', 
+  //  options: {'100' : false , '200' : true, '300' : false , '500' : false })
+  //);
+
+  db.fetchQuestion();
+
+  //the run app method
   runApp(
     const MyApp(),
   );
@@ -11,7 +24,6 @@ void main(){
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) :super(key: key);
 
-
   @override
   Widget build(BuildContext context){
     return const MaterialApp(
@@ -20,6 +32,4 @@ class MyApp extends StatelessWidget {
       home: HomeScreen(),
     );
   }
-
-
 }
--- lib/models/db_connect.dart
+++ lib/models/db_connect.dart
@@ -1,12 +1,31 @@
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import './question_model.dart';
+import 'dart:convert';
 
 
-class DbConnect {
-  final url = Uri.parse('https://simplequizapp-2b5df-default-rtdb.firebaseio.com/');
+class Dbconnect {
+  final url = Uri.parse('https://#####.com/questions.json');
 
   Future<void> addQuestion(Question question) async {
-    http.post(url);
+    http.post(url, 
+      body: json.encode({
+        'title' : question.title,
+        'options' : question.options,
+      })
+    );
+  }
+
+  //fetch the data from database
+  Future<void> fetchQuestion() async {
+    //http.get(url);
+
+    //decode it first
+    http.get(url).then((response) {
+      var data = json.decode(response.body);
+      print(data);
+
+    });
   }
 
 }
